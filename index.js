db5efb18f05eee8bf9d1116f804ae42bde2851df
Zoe <buckzoe5@gmail.com>
2022-01-16 17:32:05 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400)
+  return ('This one is on me!')
+
+  if (someValue > 2000 && someValue <= 2500)
+  return ('I will gladly take your thirty bucks.')
+
+  if (someValue > 2500)
+  return ('No can do.')
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return ('Thank you so much.');
+    case 'not as generous':
+      return ('Thank you.')
+    default:
+      return ('Bye.')
+
+      
+  }
 }
